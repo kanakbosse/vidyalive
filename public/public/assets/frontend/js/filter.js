85e7d8ba4b497e39aa9d931d30d34c421ce983c2
kanakbosse <kanchantti83@gmail.com>
2025-01-08 12:41:25 +0530

    Delete public directory

--- public/public/assets/frontend/js/filter.js
+++
@@ -1,8 +0,0 @@
-    // JavaScript to handle the accordion toggle functionality
-document.querySelectorAll('.accordion-toggle').forEach(button => {
-button.addEventListener('click', function () {
-const parentItem = this.closest('.accordion-item');
-parentItem.classList.toggle('open'); // Toggle the open class to show/hide the content
-});
-});
-
